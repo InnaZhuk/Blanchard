9282df802fe8bdd3ae591753546e25873bf95bf6
InnaZhuk <inna.zhuk@tut.by>
2023-05-21 13:34:03 +0300

    V3

--- js/index.js
+++ js/index.js
@@ -409,26 +409,6 @@ navItems.forEach(el => {
   });
 });
 
-// const burger = document?.querySelector('[data-burger]');
-// const nav = document?.querySelector('[data-nav]');
-// const body = document.body;
-// const navItems = nav?.querySelectorAll('a');
-
-
-// burger?.addEventListener('click', () => {
-//   body.classList.toggle('stop-scroll');
-//   burger?.classList.toggle('burger--active');
-//   nav?.classList.toggle('nav--visible');
-// });
-
-// navItems.forEach(el => {
-//   el.addEventListener('click', () => {
-//     body.classList.remove('stop-scroll');
-//     burger?.classList.remove('burger--active');
-//     nav?.classList.remove('nav--visible');
-//   });
-// });
-
 // Search
 
 const search = document?.querySelector('[data-search]');
